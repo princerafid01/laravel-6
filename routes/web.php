6030e2858b15f019e3573f7eeadf3bb542c01bea
Rafid Adnan <princerafid01@gmail.com>
2019-11-17 19:32:17 +0600

    pratice php crawl

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,18 @@
 */
 
 use App\User;
+use Goutte\Client;
 
 Route::get('/', function () {
     $data['l'] = User::all();
-    return view('welcome',$data);
+    return view('welcome', $data);
+});
+Route::get('/scrap', function () {
+    $client = new Client();
+    $crawler = $client->request('GET', "https://laravel-news.com/");
+    $crawler->filter('a.text-white')->each(function ($node) {
+        // dd($node);
+        $headline = $node->text();
+        echo $headline.'<br>';
+    });
 });
